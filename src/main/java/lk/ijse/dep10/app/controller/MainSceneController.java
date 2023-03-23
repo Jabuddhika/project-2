f43eaa7b4f705dedb2e506c64dbcbb55f40c8947
Isuru <isurusajith0@gmail.com>
2023-03-23 18:25:22 +0530

    very first commit

---
+++ src/main/java/lk/ijse/dep10/app/controller/MainSceneController.java
@@ -0,0 +1,41 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainSceneController {
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnEmployees;
+
+    @FXML
+    private Button btnStudent;
+
+    @FXML
+    private Button btnTeachers;
+
+    @FXML
+    void btnCustomersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTeachersOnAction(ActionEvent event) {
+
+    }
+
+}
