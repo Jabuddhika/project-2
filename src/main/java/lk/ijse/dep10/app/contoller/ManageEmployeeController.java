ca1f965dd89272fdb91a0c5ce2da47c63f6e1314
jabsampath <jab.sampath@gmil.com>
2023-03-23 19:28:11 +0530

    forth commit

--- src/main/java/lk/ijse/dep10/app/contoller/ManageEmployeeController.java
+++ src/main/java/lk/ijse/dep10/app/contoller/ManageEmployeeController.java
@@ -5,7 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import lk.ijse.dep10.app.contoller.mod.DBConnection;
 import lk.ijse.dep10.app.contoller.util.Employee;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class ManageEmployeeController {
 
@@ -37,11 +41,26 @@ public class ManageEmployeeController {
 
     @FXML
     void btnNewEmployeeOnAction(ActionEvent event) {
+        txtId.clear();
+        txtName.clear();
+        txtId.clear();
+        generateNewId();
 
+    }
+
+    private void generateNewId() {
+        if (tblEmployee.getItems().isEmpty()) {
+            txtId.setText("S001");
+            return;
+        }
+       String newId= (Integer.parseInt(tblEmployee.getItems().get(tblEmployee.
+                getItems().size() - 1).getId().substring(1)) + 1)+"";
+        txtId.setText("S00"+newId);
     }
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
+        Connection connection= DBConnection.getInstance().getConnection();
 
     }
 
