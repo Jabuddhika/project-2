768b8bf748760d1fb76ee3f8a2082b5f62f90e44
yasiru <yasiru.hirantha@gmail.com>
2023-03-23 18:43:43 +0530

    This is the first commit from Yasiru

---
+++ src/main/java/lk/ijse/dep10/app/controller/ManageCustomerController.java
@@ -0,0 +1,70 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class ManageCustomerController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewCustomer;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableColumn<?, ?> tblCustomer;
+
+    @FXML
+    private TableColumn<?, ?> tblCustomers;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAddressOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNameOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/lk/ijse/dep10/app/controller/ManageStudentController.java
@@ -0,0 +1,67 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageStudentController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewStudent;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableView<?> tblStudent;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAddressOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNameOnAction(ActionEvent event) {
+
+    }
+
+}
