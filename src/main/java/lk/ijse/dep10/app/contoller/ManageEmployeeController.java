12766f32c1c1b05a9f83db0414898a2bd3aea0a4
jabsampath <jab.sampath@gmil.com>
2023-03-23 18:48:44 +0530

    second commit

---
+++ src/main/java/lk/ijse/dep10/app/contoller/ManageEmployeeController.java
@@ -0,0 +1,47 @@
+package lk.ijse.dep10.app.contoller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageEmployeeController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewEmployee;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableView<?> tblEmployee;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewEmployeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/lk/ijse/dep10/app/contoller/ManageTeachersController.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep10.app.contoller;
+
+public class ManageTeachersController {
+}
